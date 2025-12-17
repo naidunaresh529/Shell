6e0b8ce0bfb2c48291f9635f46482b21c9281fb1
Naresh <naidunaresh529@gmail.com>
2025-12-17 16:52:42 +0530

    new file:   ramesh.sh

---
+++ ramesh.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "ramesh:: hi suresh, how are you?"
+echo "suresh:: hi ramesh, I am fine. how about you?"
+echo "ramesh:: I am also fine. what are you doing thesedays?"
+echo "suresh:: I am learning bash scripting. it's fun!"
