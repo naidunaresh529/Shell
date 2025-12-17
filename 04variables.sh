cd60ad4d90045a08981a1d28beada21b11e01212
Naresh <naidunaresh529@gmail.com>
2025-12-17 21:56:10 +0530

    some message

---
+++ 04variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Arguments passed from command line
+PERSON1=$1
+PERSON2=$2
+echo "trump:: Welcome to Alaska Putin"
+echo "putin:: Thank you Trump, it's great to be here!"
+echo "trump:: How do you like Alaska so far?"
+echo "putin:: It's beautiful, the landscapes are stunning!"
---
+++ 05variables.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "please enter your pin number:"
+read PIN_NUMBER
+echo "You entered PIN number: $PIN_NUMBER"
