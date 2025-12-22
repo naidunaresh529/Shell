3498d20785b1637086e7b6804b2d3a54bf6c3434
Naresh <naidunaresh529@gmail.com>
2025-12-22 13:06:34 +0530

    moksha

---
+++ 5varanasi.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Reading from a file and assigning to variables 
+naresh=$f1
+barath=$f2
+echo "Naresh weds : $naresh"
+echo "Barath weds : $barath"
+echo "Wedding Invitations Sent Successfully"
