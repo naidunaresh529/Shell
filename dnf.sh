5cf5d61d65e185aeaaa745d27b5c5da5f82a7807
Naresh <naidunaresh529@gmail.com>
2025-12-27 12:33:08 +0530

    somemessage

--- dnf.sh
+++ dnf.sh
@@ -3,12 +3,23 @@
 USERID=$(id -u)
 if [$USERID -ne 0 ]; then
 echo "Error:: Please run this script with root previlage"
+exit 1
 fi
 
 dnf install mongodb -y
 if [$? -ne 0 ];then
     echo "Error:: Installing mongodb server"
+    exit 1
     else
     echo "Installing mongodb is success"
+    exit 1
     fi
 
+dnf install nginx -y
+if [$? -ne 0 ]; then
+    echo ""Error:: Installing nginx server"
+    exit 1
+    else
+    echo "Installing nginx is success"
+    exit 1
+    fi
